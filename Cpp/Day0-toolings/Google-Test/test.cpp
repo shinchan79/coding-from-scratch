4956941e60629965f3fe8c568abc4f83d1b2a937
Yen Trinh <trinhhaiyen79@gmail.com>
2026-08-26 16:40:03 +0700

    add lab4 day0

---
+++ Cpp/Day0-toolings/Google-Test/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yentrinh on 26/8/26.
+//
+#include <gtest/gtest.h>
+
+// Empty test, check a condition that is always true
+TEST(Day0_Lab4, EmptyTest) {
+  EXPECT_TRUE(true);
+  EXPECT_EQ(1 + 1, 2);
+}
